f7bf13b74df5a99f8659545c5e5a688a8876baa3
theh2ok <huascar_hho@hotmail.com>
2020-02-01 11:47:40 -0400

    subida de efectos y assets de gliches

---
+++ Assets/miradletal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miradletal : MonoBehaviour
+{
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        var cameraCenter = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, camera.nearClipPlane));
+        if (Physics.Raycast(cameraCenter, this.transform.forward, out hit, 1000))
+        {
+            var obj = hit.transform.gameObject;
+            Debug.Log(obj);
+        }
+    }
+ 
+    }
