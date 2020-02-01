f65bd1a0aa5b46d47eb84b676981f8a0e1d13fea
Widrogo <amils21@gmail.com>
2020-02-01 02:06:04 -0400

    Alza y suelta objetos

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rigidBody3D;
+    public Transform onhand;
+    void Start()
+    {
+        rigidBody3D = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        rigidBody3D.useGravity = false;
+        transform.position = onhand.position;
+        transform.parent = GameObject.Find("Hand").transform;
+    }
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        rigidBody3D.useGravity = true;
+    }
+}
