2c9027c250245cd70225f6bc4742f59737095de4
Widrogo <amils21@gmail.com>
2020-02-02 12:09:16 -0400

    condicion victoria

--- Assets/App_Husk/sc/completomostro.cs
+++ Assets/App_Husk/sc/completomostro.cs
@@ -2,31 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class completomostro : MonoBehaviour {
+public class completomostro : MonoBehaviour
+{
     public bool completo;
     int partes_c = 0;
     Rigidbody mio_rig;
-    public List<GameObject> obj = new List<GameObject> ();
+    public List<GameObject> obj = new List<GameObject>();
     // Start is called before the first frame update
-    void Start () {
-        mio_rig = GetComponent<Rigidbody> ();
+    GameStatus gameStatus;
+    void Start()
+    {
+        mio_rig = GetComponent<Rigidbody>();
+        gameStatus = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameStatus>();
     }
 
     // Update is called once per frame
-    public void partes (GameObject x) {
+    public void partes(GameObject x)
+    {
 
         partes_c = partes_c + 1;
-        obj.Add (x);
-        if (partes_c >= 6) {
+        obj.Add(x);
+        if (partes_c >= 6)
+        {
             completo = true;
-            vida_itsalife ();
-            foreach (var item in obj) {
+            gameStatus.WinState = true;
+            vida_itsalife();
+            foreach (var item in obj)
+            {
                 //   item.GetComponent<BoxCollider>().enabled=true;
             }
         }
     }
-    public void vida_itsalife () {
-        GetComponent<BoxCollider> ().enabled = true;
+    public void vida_itsalife()
+    {
+        GetComponent<BoxCollider>().enabled = true;
         mio_rig.isKinematic = false;
         mio_rig.useGravity = true;
         // mio_rig.AddForce(Vector3.forward*455,ForceMode.Force);
