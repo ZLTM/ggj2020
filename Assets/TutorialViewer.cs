ed2cc41d1d4f35e56c48d02e961f7afa2141a544
zltm <jdavidrevillad@gmail.com>
2020-02-02 01:14:12 -0400

    small wall fix, comments deleted

--- Assets/TutorialViewer.cs
+++ Assets/TutorialViewer.cs
@@ -9,7 +9,6 @@ public class TutorialViewer : MonoBehaviour
     public Image TutorialImage;
     public GameObject Player;
     public GameObject Enemy;
-    // Start is called before the first frame update
     void Start()
     {
         TutorialImage.GetComponent<Image>().enabled = true;
@@ -17,7 +16,6 @@ public class TutorialViewer : MonoBehaviour
         Enemy.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.anyKeyDown)
